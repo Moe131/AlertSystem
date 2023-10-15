286ef03ac0874ff9b6416c65c6e84abc4a3aa045
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2023-10-15 10:49:14 -0700

    Input file lines are successfully read and returned as a list

--- project1.py
+++ project1.py
@@ -5,10 +5,15 @@ def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
     return Path(input())
 
+def readFileLines(path:Path, encoding = 'utf-8') -> list:
+    """Reads the lines of a file with given path and returns them as a list"""
+    with open(path, 'r', encoding = encoding) as the_file:
+        return the_file.readlines()
 
 def main() -> None:
     """Runs the simulation program in its entirety"""
     input_file_path = _read_input_file_path()
+    inputLines = readFileLines(input_file_path)
 
 
 if __name__ == '__main__':
--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,3 +1,22 @@
+from pathlib import Path
 import unittest
+import project1
+
+class project1Test(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self._path = Path('../samples/sample_input.txt')
+
+    def test_file_lines_are_returned_as_a_string(self):
+        with open(self._path, 'r', encoding = 'utf-8') as the_file:
+            self.assertEqual(project1.readFileLines(self._path), the_file.readlines())
+
+    # This can not be tested since the function requires user input from terminal
+    #
+    #def test_correct_input_file_path_is_returned(self):
+    #    path = Path('../samples/sample_input.txt')
+    #    self.assertEqual(project1._read_input_file_path(),path)
 
 
+if __name__ == '__main__':
+    unittest.main()
