c546579cd96b17253c7beeb9000826f8b4d942b5
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2023-10-15 11:34:49 -0700

    Simulation class can successfully change length

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,6 +5,10 @@ class simulationTest(unittest.TestCase):
     def setUp(self) -> None:
         self._sim = Simulation()
 
+    def test_simulation_length_is_changed_successfully(self):
+        self._sim.setLength(1000)
+        self.assertEqual(self._sim.getLength(), 1000)
+
 
 if __name__ == '__main__':
     unittest.main()
