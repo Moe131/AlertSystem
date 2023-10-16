fb4af8fa8f35487070d55d86944c66aa0854d79a
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2023-10-15 23:54:42 -0700

    Event class test units were added

--- tests/test_event.py
+++ tests/test_event.py
@@ -16,6 +16,15 @@ class eventTest(unittest.TestCase):
     def test_event_type_is_returned(self):
         self.assertEqual(self._event.getType(), 'SENT')
 
+    def test_event_toString_method_returns_correct_format(self):
+        event = Event(0, 1, 2, Alert("Trouble"), 'SENT')
+        self.assertEqual(event.toString(), "@0: #1 SENT ALERT TO #2: Trouble")
+        event = Event(0, 1, 2, Alert("Trouble"), 'RECEIVED')
+        self.assertEqual(event.toString(), "@0: #2 RECEIVED ALERT FROM #1: Trouble")
+        event = Event(0, 1, 2, Alert("Trouble", True), 'SENT')
+        self.assertEqual(event.toString(), "@0: #1 SENT CANCELLATION TO #2: Trouble")
+        event = Event(0, 1, 2, Alert("Trouble", True), 'RECEIVED')
+        self.assertEqual(event.toString(), "@0: #2 RECEIVED CANCELLATION FROM #1: Trouble")
 
 if __name__ == '__main__':
     unittest.main()
